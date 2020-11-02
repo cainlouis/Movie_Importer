547c3fd7c0718f1e3de388340f3ad51cab275852
Juan-Carlos Sreng-Flores <srengfloresj@hotmail.com>
2020-11-02 14:47:09 -0500

    Edited toString method

--- eclipse/src/movies/importer/Movie.java
+++ eclipse/src/movies/importer/Movie.java
@@ -30,7 +30,12 @@ public class Movie {
 	}
 	
 	public String toString() {
-		return (this.name + "\t" + this.releaseYear + "\t" + this.runtime + "\t" + this.source);
+		StringBuilder s = new StringBuilder();
+		s.append("Movie name: "+this.name);
+		s.append("\nRelease year: "+this.releaseYear);
+		s.append("\nRuntime: "+this.runtime);
+		s.append("\nSource: "+this.source);
+		return s.toString();
 	}
 	
 }
