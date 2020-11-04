578bbaf9541076402267841c7d6d35ee8278ab1f
Juan-Carlos Sreng-Flores <srengfloresj@hotmail.com>
2020-11-03 23:29:26 -0500

    Comment on Movie.java

--- eclipse/src/movies/importer/Movie.java
+++ eclipse/src/movies/importer/Movie.java
@@ -12,23 +12,56 @@ public class Movie {
 		this.runtime = runtime;
 		this.source = source;
 	}
-	
+	/**
+	 * @author Nael Louis
+	 * This method is a getter method for releaseYear attribute 
+	 * of the Movie class.
+	 * 
+	 * @return the releaseYear attribute.
+	 * */
 	public String getReleaseYear() {
 		return this.releaseYear;
 	}
-	
+	/**
+	 * @author Nael Louis
+	 * This method is a getter method for name attribute 
+	 * of the Movie class.
+	 * 
+	 * @return the name attribute.
+	 * */
 	public String getName() {
 		return this.name;
 	}
-	
+	/**
+	 * @author Nael Louis
+	 * This method is a getter method for runtime attribute 
+	 * of the Movie class.
+	 * 
+	 * @return the runtime attribute.
+	 * */
 	public String getRuntime() {
 		return this.runtime;
 	}
-	
+	/**
+	 * @author Nael Louis
+	 * This method is a getter method for source attribute 
+	 * of the Movie class.
+	 * 
+	 * @return the source attribute.
+	 * */
 	public String getSource() {
 		return this.source;
 	}
-	
+	/**
+	 * @author Nael Louis
+	 * @author Juan-Carlos Sreng-Flores 
+	 * This method overrides toString() method from Object class
+	 * It uses a StringBuilder in order to create a string concatenation
+	 * StringBuilder is used in case there a many calls of toString method.
+	 * 
+	 * @return the String version of the Movie Object.
+	 * */
+	@Override
 	public String toString() {
 		StringBuilder s = new StringBuilder();
 		s.append("Movie name: "+this.name);
