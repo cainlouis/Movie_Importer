fc3af8a9fada5992cf88329133d086de0231ddaa
Juan-Carlos Sreng-Flores <srengfloresj@hotmail.com>
2020-11-04 09:53:46 -0500

    Finished MovieTests.java

--- eclipse/src/movies/tests/MovieTests.java
+++ eclipse/src/movies/tests/MovieTests.java
@@ -21,29 +21,41 @@ class MovieTests {
 	 * This method tests the getter method for the name attribute.*/
 	@Test
 	void testGetName() {
-		
+		Movie movie = new Movie("2016", "Moana", "Any runtime", "C:\\MyDirectory\\AnotherDirectory");
+		assertEquals("Moana", movie.getName());
 	}
 	
 	/**
 	 * @author Juan-Carlos Sreng-Flores
 	 * @author Nael Louis
-	 * This method tests the getter method for the name attribute.*/
+	 * This method tests the getter method for the runtime attribute.*/
 	@Test
 	void testGetRuntime() {
-		
+		Movie movie = new Movie("2016", "Moana", "Any runtime", "C:\\MyDirectory\\AnotherDirectory");
+		assertEquals("Any runtime", movie.getRuntime());
 	}
 	
 	/**
 	 * @author Juan-Carlos Sreng-Flores
 	 * @author Nael Louis
-	 * This method tests the getter method for the name attribute.*/
+	 * This method tests the getter method for the source attribute.*/
 	@Test
 	void testGetSource() {
-		
+		Movie movie = new Movie("2016", "Moana", "Any runtime", "C:\\MyDirectory\\AnotherDirectory");
+		assertEquals("C:\\MyDirectory\\AnotherDirectory", movie.getSource());
 	}
 	
+	/**
+	 * @author Juan-Carlos Sreng-Flores
+	 * @author Nael Louis
+	 * This method tests the overridden toString() method in the Movie class.
+	 * The toString method is expected to return a string similar to the following:
+	 * 	"releaseYear\tname\truntime\tsource"
+	 * 
+	 * */
 	@Test 
 	void testToString() {
-		
+		Movie movie = new Movie("2016", "Moana", "Any runtime", "C:\\MyDirectory\\AnotherDirectory");
+		assertEquals("2016\tMoana\tAny runtime\tC:\\MyDirectory\\AnotherDirectory", movie.toString());
 	}
 }
