e4a92edb8eca4b5e228474e3baf82765bc35e54d
Juan-Carlos Sreng-Flores <srengfloresj@hotmail.com>
2020-11-04 09:43:16 -0500

    Added tests methods in MovieTests.java

--- eclipse/src/movies/tests/MovieTests.java
+++ eclipse/src/movies/tests/MovieTests.java
@@ -6,10 +6,44 @@ import org.junit.jupiter.api.Test;
 
 class MovieTests {
 
+	/** @author Juan-Carlos Sreng-Flores
+	 * 	@author Nael Louis
+	 * This method tests the getter Method for the releaseYear attribute*/
 	@Test
-	void test() {
-		Movie movie = new Movie("Moana", "", "Hel", "Hello");
-		fail("Not yet implemented");
+	void testGetReleaseYear() {
+		Movie movie = new Movie("2016", "Moana", "Any runtime", "C:\\MyDirectory\\AnotherDirectory");
+		assertEquals("2016", movie.getReleaseYear());
 	}
-
+	
+	/**
+	 * @author Juan-Carlos Sreng-Flores
+	 * @author Nael Louis
+	 * This method tests the getter method for the name attribute.*/
+	@Test
+	void testGetName() {
+		
+	}
+	
+	/**
+	 * @author Juan-Carlos Sreng-Flores
+	 * @author Nael Louis
+	 * This method tests the getter method for the name attribute.*/
+	@Test
+	void testGetRuntime() {
+		
+	}
+	
+	/**
+	 * @author Juan-Carlos Sreng-Flores
+	 * @author Nael Louis
+	 * This method tests the getter method for the name attribute.*/
+	@Test
+	void testGetSource() {
+		
+	}
+	
+	@Test 
+	void testToString() {
+		
+	}
 }
