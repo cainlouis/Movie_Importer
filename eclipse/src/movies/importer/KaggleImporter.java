c67db1dd504b5e506dc2f0e7f3b6a6e240c34d17
Nael Louis <nael.louis1002@gmail.com>
2020-11-04 20:01:54 -0500

    Modifications made to class Normalizer and class KaggleImporter

--- eclipse/src/movies/importer/KaggleImporter.java
+++ eclipse/src/movies/importer/KaggleImporter.java
@@ -2,12 +2,26 @@ package movies.importer;
 
 import java.util.ArrayList;
 
+/**
+ * A class that extends an abstract class to process an ArryList from a source directory
+ * @author Nael Louis
+ *
+ */
 public class KaggleImporter extends Processor {
 	
 	public KaggleImporter(String source, String destination) {
 		super(source, destination, true);
 	}
 	
+	/**
+	 * Gets an ArrayList called input from a source directory and creates an ArrayList named processed that will contain 
+	 * all of the Strings in input once processed. 
+	 * To do so, the Strings are separated where there is a tab space and put into a String array. 
+	 * The index of the first string, which contains the name of each column, containing the Runtime, the title, and the year are 
+	 * stored into a integer variable to create a new movie object.
+	 * Then the toString method from the class Movie is used to add the movie object to the processed Arraylist which is then returned
+	 * once all the string of input has been processed.  
+	 */
 	public ArrayList<String> process(ArrayList<String> input) {
 		int columnNum = 0;
 		int nameIndex = 0; 
@@ -17,6 +31,7 @@ public class KaggleImporter extends Processor {
 		String[] separated = input.get(0).split("\\t");
 		
 		columnNum = separated.length;
+		//Find the index of the column necessary to create a movie object
 		for (int i = 0; i < columnNum; i++) {
 			if (separated[i] == "Runtime") {
 				runtimeIndex = i;
@@ -29,8 +44,10 @@ public class KaggleImporter extends Processor {
 			}
 		}
 		
+		//Create a movie object and add the string from toString() to the processed ArrayList 
 		for (int i = 0; i < input.size(); i++) {
 			separated = input.get(i).split("\\t");
+			//if the amount of column is correct create the object
 			if (separated.length == columnNum) {
 				Movie newMovie = new Movie(separated[yearIndex], separated[nameIndex], separated[runtimeIndex], "kaggle");
 				processed.add(newMovie.toString());
@@ -38,5 +55,6 @@ public class KaggleImporter extends Processor {
 		}
 		return processed;
 	}
+	
 	
 }
