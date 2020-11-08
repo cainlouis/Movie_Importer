f7da04ee023df92a61c18296ce28f31bd383770f
Juan-Carlos Sreng-Flores <srengfloresj@hotmail.com>
2020-11-08 02:04:39 -0500

    Comments on ImportPipeline.java

--- eclipse/src/movies/importer/ImportPipeline.java
+++ eclipse/src/movies/importer/ImportPipeline.java
@@ -8,8 +8,13 @@ public class ImportPipeline {
 		String srcDirValidator = "";
 		String srcDirDeduper = "";
 		String outputDirFinal = "";
-		
+		//create Processor[] and call processAll(); 
 	}
+	/**
+	 * @author Juan-Carlos Sreng-Flores
+	 * @param importers - Processor[] where all the concrete classes that extends processor are put into it.
+	 * This method will execute all the Processor Objects in order to complete the pipeline.
+	 */
 	public static void processAll(Processor[] importers) throws IOException{
 		for(Processor importer : importers) {
 			importer.execute();
